e9c5485294b6d7ec5d769e4367a817104545c379
Scott Meade <scott@synapsoftware.com>
2018-02-08 17:32:38 -0700

    Use SendGrid to deliver transactional emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }  
@@ -58,4 +59,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => Rails.application.credentials.sendgrid[:username],
+    :password => Rails.application.credentials.sendgrid[:password],
+    :domain => 'blog2018.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+    
 end
